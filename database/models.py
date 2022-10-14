1dceab2ae117b58702a1b61d1b07089bf05c87a3
sanjarbek <sanjarsaidov173@gmail.com>
2022-10-14 11:15:40 +0500

    created models

--- database/models.py
+++ database/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.first_name
+
+class Books(models.Model):
+    title = models.CharField(max_length=50)
+    total_pages = models.DecimalField()
+    published_date = models.DateField(auto_now_add=True)
+    genres = models.CharField(max_lenght=30)
+    author = models.ForeignKey(Author)
+
+    def __str__(self):
+        return self.title
